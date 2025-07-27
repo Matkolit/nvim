d35179bc4fd56a368f3052b53069b50432882120
Mateusz Kołodziej <mateusz@net47.pl>
2025-07-27 17:12:16 +0200

    feat(visual): add line copy highlight, disable missing error fields

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,24 @@
-require "nvchad.autocmds"
+require("nvchad.autocmds")
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local yank_group = augroup("YankHighLight", {})
+local mati_group = augroup("MatiGroup", {})
+
+autocmd("TextYankPost", {
+  group = yank_group,
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 40,
+    })
+  end,
+})
+
+autocmd("BufWritePre", {
+  group = mati_group,
+  pattern = "*",
+  command = [[%s/\s\+$//e]], -- remove trailing whitespacev
+})
